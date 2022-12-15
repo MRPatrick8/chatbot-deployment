77c644a2cf1b1272ad02f879327e336a2b818361
MRPatrick8 <115985374+MRPatrick8@users.noreply.github.com>
2022-12-15 14:37:59 +0200

    Add files via upload

--- chat.py
+++ chat.py
@@ -25,7 +25,7 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Maguire"
+bot_name = "Sam"
 
 def get_response(msg):
     sentence = tokenize(msg)
@@ -48,14 +48,14 @@ def get_response(msg):
     return "I do not understand..."
 
 
-# if __name__ == "__main__":
-#     print("Let's chat! (type 'quit' to exit)")
-#     while True:
-#         # sentence = "do you use credit cards?"
-#         sentence = input("You: ")
-#         if sentence == "quit":
-#             break
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
 
-#         resp = get_response(sentence)
-#         print(resp)
+        resp = get_response(sentence)
+        print(resp)
 
