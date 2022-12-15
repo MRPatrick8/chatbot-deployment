63c0c2a1fd2b7bae701ac4bd82d58c380bf3380f
MRPatrick8 <115985374+MRPatrick8@users.noreply.github.com>
2022-12-15 16:41:37 +0200

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -45,8 +45,8 @@ class Chatbox {
 
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
-
-        fetch('http://127.0.0.1:5000/predict', {
+        // # http://127.0.0.1:5000
+        fetch($SCRIPT_ROOT + '/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
