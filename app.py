67832920aba183fecf97ee359018a285c4b6f4d7
MRPatrick8 <115985374+MRPatrick8@users.noreply.github.com>
2022-12-16 14:41:11 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,31 +7,6 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-
-
-# class translator:
-#     api_url = "https://translate.googleapis.com/translate_a/single"
-#     client = "?client=gtx&dt=t"
-#     dt = "&dt=t"
-
-#     #fROM English to Kinyarwanda
-#     def translate(text : str , target_lang : str, source_lang : str):
-#         sl = f"&sl={source_lang}"
-#         tl = f"&tl={target_lang}"
-#         r = requests.get(translator.api_url+ translator.client + translator.dt + sl + tl + "&q=" + text)
-#         return json.loads(r.text)[0][0][0]
-
-
-# def process_question(text : str):
-
-#   source_lang = detect(text)
-#   resp = translator.translate(text=text, target_lang='en', source_lang=source_lang)
-#   return resp, source_lang
-
-# def process_answer(text : str, source_lang):
-#   resp = translator.translate(text=text, target_lang=source_lang, source_lang='en')
-#   return resp
-
 # create two routes
 def preprocessing(text):
     text = text.lower()
@@ -54,16 +29,13 @@ def index_get():
 
 def predict():
     text = request.get_json().get("message")
-    text, SL = process_question(text)
     #check if text is valid (I let it for you)
     response = preprocessing(text)
-#     response = get_response(texte)
     # we jsonify our response
     response = get_response(response)
-#     response = process_answer(response, SL)
     message = {"answer":response}
 
     return jsonify(message)
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
